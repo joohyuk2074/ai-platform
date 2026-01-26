50c90977654ec02dc16b39e7e21e72a48193acc4
joohyuk2074 <vfrvfr4207@gmail.com>
2026-01-26 21:12:23 +0900

    Initial Commit

---
+++ src/test/java/me/joohyuk/aiplatform/AiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joohyuk.aiplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlatformApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
